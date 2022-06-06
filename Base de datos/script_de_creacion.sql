3c770a9f807062341a7aeff4027764668249cb1d
Facu-hub-code <68453608+Facu-hub-code@users.noreply.github.com>
2022-06-06 17:29:29 -0300

    dev: script de creacion bdd

---
+++ Base de datos/script_de_creacion.sql
@@ -0,0 +1,33 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema bdd_BorbosBlock
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `bdd_BorbosBlock` ;
+
+-- -----------------------------------------------------
+-- Schema bdd_BorbosBlock
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bdd_BorbosBlock` DEFAULT CHARACTER SET utf8 ;
+USE `bdd_BorbosBlock` ;
+
+-- -----------------------------------------------------
+-- Table `bdd_BorbosBlock`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdd_BorbosBlock`.`usuarios` (
+  `idusuarios` INT NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusuarios`))
+ENGINE = InnoDB;
+
+INSERT INTO `bdd_borbosblock`.`usuarios` (`idusuarios`, `username`, `password`) VALUES ('0', 'admin', 'admin');
+INSERT INTO `bdd_borbosblock`.`usuarios` (`idusuarios`, `username`, `password`) VALUES ('1', 'root', 'toor');
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
